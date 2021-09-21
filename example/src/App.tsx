17769cb916deab8498709f69b4b8d05b9b3ae6c5
Quoc Khanh <bkdev98@gmail.com>
2021-09-21 22:36:29 +0700

    feat(example): replace scrollview with flatlist

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,7 +6,7 @@ import {
   View,
   Text,
   SafeAreaView,
-  ScrollView,
+  FlatList,
   StatusBar,
   Image,
   TouchableOpacity,
@@ -61,18 +61,19 @@ export default function App() {
           </TouchableOpacity>
         </View>
       </SafeAreaView>
-      <ScrollView style={styles.scrollView}>
-        <View style={styles.main}>
-          {Object.entries(Icons)
-            .filter(([, Icon]) => !!Icon)
-            .map(([name, Icon]) => (
-              <View key={name} style={styles.iconItem}>
-                <Icon size={40} weight={weight} />
-                <Text style={styles.iconName}>{name}</Text>
-              </View>
-            ))}
-        </View>
-      </ScrollView>
+      <FlatList
+        style={styles.scrollView}
+        contentContainerStyle={styles.main}
+        data={Object.entries(Icons).filter(([, Icon]) => !!Icon) as any[]}
+        keyExtractor={(item) => item[0]}
+        numColumns={3}
+        renderItem={({ item: [name, Icon] }) => (
+          <View key={name} style={styles.iconItem}>
+            <Icon size={40} weight={weight} />
+            <Text style={styles.iconName}>{name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -108,13 +109,11 @@ const styles = StyleSheet.create({
   weightSelect: {
     width: 40,
   },
-  scrollView: {},
-  main: {
+  scrollView: {
     flex: 1,
+  },
+  main: {
     backgroundColor: 'white',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    width: '100%',
     paddingHorizontal: 8,
     paddingBottom: 16,
   },
