a8da224c7550c7d5d2df3fee65910b3e712fe9c3
Dustin Do <dustin.do95@gmail.com>
2021-09-21 15:45:52 +0700

    feat(example): add weight switcher

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 
 import {
@@ -8,6 +9,7 @@ import {
   ScrollView,
   StatusBar,
   Image,
+  TouchableOpacity,
 } from 'react-native';
 import * as IconPack from 'phosphor-react-native';
 const PhosphorLogo = require('./phosphor-mark-tight-yellow.png');
@@ -15,7 +17,17 @@ const PhosphorLogo = require('./phosphor-mark-tight-yellow.png');
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { IconContext, ...Icons } = IconPack;
 
+const weights = ['thin', 'light', 'regular', 'bold', 'fill', 'duotone'];
+
 export default function App() {
+  const [weightIdx, setWeightIdx] = React.useState(2);
+
+  const weight = React.useMemo(() => weights[weightIdx], [weightIdx]);
+
+  const handleChangeWeight = React.useCallback(() => {
+    setWeightIdx((weightIdx + 1) % weights.length);
+  }, [weightIdx]);
+
   return (
     <View style={styles.rootView}>
       <StatusBar barStyle="light-content" />
@@ -23,10 +35,30 @@ export default function App() {
       <SafeAreaView style={styles.headerContainer}>
         <View style={styles.header}>
           <Image source={PhosphorLogo} style={styles.logoImage} />
-          <Text style={styles.headerText}>Phosphor React Native</Text>
-          <View style={styles.weightSelect}>
-            <IconPack.PencilLine color="#FFF" />
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={styles.headerText}>Phosphor React Native</Text>
+            <Text
+              style={{
+                color: '#fff',
+                opacity: 0.8,
+                textTransform: 'capitalize',
+              }}
+            >
+              {weight}
+            </Text>
           </View>
+          <TouchableOpacity
+            style={styles.weightSelect}
+            onPress={handleChangeWeight}
+          >
+            <IconPack.PencilLine color="#FFF" weight={weight} />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
       <ScrollView style={styles.scrollView}>
@@ -35,7 +67,7 @@ export default function App() {
             .filter(([, Icon]) => !!Icon)
             .map(([name, Icon]) => (
               <View key={name} style={styles.iconItem}>
-                <Icon size={40} />
+                <Icon size={40} weight={weight} />
                 <Text style={styles.iconName}>{name}</Text>
               </View>
             ))}
