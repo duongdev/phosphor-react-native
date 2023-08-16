b418a989880cedc2f55440ab30dd97af3c725fa4
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2023-08-16 10:55:52 +0100

    fix: add instructions for tsc errors

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,4 @@
-import { SvgProps as DefaultSvgProps } from 'react-native-svg';
+import type { SvgProps as DefaultSvgProps } from 'react-native-svg';
 
 declare module 'react-native-svg' {
   interface SvgProps extends DefaultSvgProps {
