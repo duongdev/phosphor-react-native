648e42387f02bbd18f95b833c6841fe3c8436697
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2025-09-28 00:20:59 +0100

    fix: upgrade dependencies + expo example to 54

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -6,7 +6,6 @@ import {
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
